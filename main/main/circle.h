c31a8303bb01e800a796e93f9e67c68f74e7ae39
Ryabova2020 <76618274+EkaEkaEkaRy@users.noreply.github.com>
2023-02-19 13:59:55 +0300

    /

--- main/main/circle.h
+++ main/main/circle.h
@@ -11,9 +11,41 @@ public:
         x_center = x;
         y_center = y;
     }
-    void set_circle(float r, float x, float y);
-    float square();
-    bool triangle_around(float a, float b, float c);
-    bool triangle_in(float a, float b, float c);
-    bool check_circle(float r, float x, float y);
+    void set_circle(float r, float x, float y)
+    {
+        radius = r;
+        x_center = x;
+        y_center = y;
+    }
+    void show()
+    {
+        std::cout << "радиус: " << radius << std::endl;
+        std::cout << "координата x: " << x_center << std::endl;
+        std::cout << "координата y: " << y_center << std::endl;
+    }
+    float square()
+    {
+        return 3.14 * radius * radius;
+    }
+    bool triangle_around(float a, float b, float c)
+    {
+        float s = 3.14 * radius * radius;
+        return a * b * c / (4 * s) == radius;
+    }
+    bool triangle_in(float a, float b, float c)
+    {
+        float s = 3.14 * radius * radius;
+        return 2 * s / (a + b + c) == radius;
+    }
+    bool check_circle(float r, float x, float y)
+    {
+        if (r < radius)
+        {
+            return sqrt((x - x_center) * (x - x_center) + (y - y_center) * (y - y_center)) < r + radius && sqrt((x - x_center) * (x - x_center) + (y - y_center) * (y - y_center)) + r >= radius;
+        }
+        else {
+            return sqrt((x - x_center) * (x - x_center) + (y - y_center) * (y - y_center)) < r + radius && sqrt((x - x_center) * (x - x_center) + (y - y_center) * (y - y_center)) + radius >= r;
+        }
+        
+    }
 };
--- main/main/main.cpp
+++ main/main/main.cpp
@@ -1,6 +1,7 @@
 ﻿#include <iostream>
 #include <cmath>
 #include "circle.h";
+#include "figure.h";
 using namespace std;
 
 
@@ -37,32 +38,11 @@ public:
 };
 
 
-
-class Figure //задание 3
-{
-private:
-    float x1, x2, x3, x4, y1, y2, y3, y4, s, p;
-public:
-    void set_figure(float x11, float x21, float x31, float x41, float y11, float y21, float y31, float y41)
-    {
-        x1 = x11;
-        x2 = x21;
-        x3 = x31;
-        x4 = x41;
-        y1 = y11;
-        y2 = y21;
-        y3 = y31;
-        y4 = y41;
-    }
-    void show()
-    {
-
-    }
-};
-
-
 int main() {
     setlocale(LC_ALL, "Russian");
+    
+    
+    // задание 1
     Triangle mas[3];
     double a, b, c;
     for (int i = 0; i < 3; i++) {
@@ -82,7 +62,7 @@ int main() {
     }
     
     
-
+    // задание 2
     cout << "Введите радиус r, координаты x, y для окружности 1 через пробел: " << endl;
     cin >> a >> b >> c;
     circle first = circle(a, b, c);
@@ -92,11 +72,54 @@ int main() {
     cout << "Введите радиус r, координаты x, y для окружности 3 через пробел: " << endl;
     cin >> a >> b >> c;
     circle third = circle(a, b, c);
+    cout << "Параметры окружности 1:" << endl;
+    first.show();
+    cout << "Параметры окружности 2:" << endl;
+    second.show();
+    cout << "Параметры окружности 3:" << endl;
+    third.show();
+    cout << "Введите ещё раз радиус r, координаты x, y для окружности 1 через пробел: " << endl;
+    cin >> a >> b >> c;
+    first.set_circle(a, b, c);
+    cout << "Параметры окружности 1:" << endl;
+    first.show();
     cout << "Площадь окружности 1: " << first.square() << endl;
     cout << "Введите стороны треугольника через пробел: " << endl;
     cin >> a >> b >> c;
     cout << "Треугольник вписан в окружность 2: " << second.triangle_around(a, b, c) << endl;
     cout << "Треугольник описан около окружности 2: " << second.triangle_in(a, b, c) << endl;
-    
+    cout << "Введите радиус r, координаты x, y для проверки пересечения окружности с окружностью 3 через пробел: " << endl;
+    cin >> a >> b >> c;
+    cout << "Окружность 3 пересекается с введенной окружностью: " << third.check_circle(a, b, c) << endl;
+
+
+    // задание 3
+    float x1, x2, x3, x4, y1, y2, y3, y4;
+    cout << "Введите координаты x1, y1 первой точки, координаты x2, y2 второй точки, координаты x3, y3 третьей точки, координаты x4, y4 четвертой точки для четырехугольнака 1 через пробел: " << endl;
+    cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3 >> x4 >> y4;
+    Figure one = Figure(x1, x2, x3, x4, y1, y2, y3, y4);
+    cout << "Введите координаты x1, y1 первой точки, координаты x2, y2 второй точки, координаты x3, y3 третьей точки, координаты x4, y4 четвертой точки для четырехугольнака 2 через пробел: " << endl;
+    cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3 >> x4 >> y4;
+    Figure two = Figure(x1, x2, x3, x4, y1, y2, y3, y4);
+    cout << "Введите координаты x1, y1 первой точки, координаты x2, y2 второй точки, координаты x3, y3 третьей точки, координаты x4, y4 четвертой точки для четырехугольнака 3 через пробел: " << endl;
+    cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3 >> x4 >> y4;
+    Figure three = Figure(x1, x2, x3, x4, y1, y2, y3, y4);
+    cout << "Параметры четырехугольника 1:" << endl;
+    one.show();
+    cout << "Четырехугольник 1 является прямоугольником: " << one.is_prug() << endl;
+    cout << "Четырехугольник 1 является квадратом: " << one.is_square() << endl;
+    cout << "Четырехугольник 1 является ромбом: " << one.is_romb() << endl;
+    cout << "Параметры четырехугольника 2:" << endl;
+    two.show();
+    cout << "Четырехугольник 2 является прямоугольником: " << two.is_prug() << endl;
+    cout << "Четырехугольник 2 является квадратом: " << two.is_square() << endl;
+    cout << "Четырехугольник 2 является ромбом: " << two.is_romb() << endl;
+    cout << "Параметры четырехугольника 3:" << endl;
+    three.show();
+    cout << "Четырехугольник 3 является прямоугольником: " << three.is_prug() << endl;
+    cout << "Четырехугольник 3 является квадратом: " << three.is_square() << endl;
+    cout << "Четырехугольник 3 является ромбом: " << three.is_romb() << endl;
+    cout << "Четырехугольник 1 можно вписать в окружность: " << one.is_in_circle() << endl;
+    cout << "Четырехугольник 2 можно описать около окружности: " << two.is_out_circle() << endl;
 }
 
