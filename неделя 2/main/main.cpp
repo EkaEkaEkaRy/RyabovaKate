ab55aa8c6d9815e64cb42799a5f0a1795ea1eb32
Ryabova2020 <76618274+EkaEkaEkaRy@users.noreply.github.com>
2023-02-25 17:45:57 +0300

    /

--- неделя 2/main/main.cpp
+++ неделя 2/main/main.cpp
@@ -2,41 +2,8 @@
 #include <cmath>
 #include "circle.h";
 #include "figure.h";
+#include "Triangle.h";
 using namespace std;
-
-
-class Triangle //задание 1
-{
-private:
-    double a, b, c;
-public:
-    bool exst_tr()
-    {
-        return ((a + b) > c) && ((a + c) > b) && ((b + c) > a);
-    }
-    void set(double a1, double b1, double c1)
-    {
-        a = a1;
-        b = b1;
-        c = c1;
-    }
-    void show()
-    {
-        cout << "1 сторона: " << a << endl;
-        cout << "2 сторона: " << b << endl;
-        cout << "3 сторона: " << c << endl;
-    }
-    double perimetr()
-    {
-        return a + b + c;
-    }
-    double square()
-    {
-        double p = (a + b + c) / 2;
-        return sqrt(p * (p - a) * (p - b) * (p - c));
-    }
-};
-
 
 int main() {
     setlocale(LC_ALL, "Russian");
